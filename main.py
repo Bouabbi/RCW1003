c7598c262f6ddee5cbac4836b92bd8dc2a438aab
APACHE <bouabbimostapha@gmail.com>
2025-06-04 14:59:06 -0400

    add client

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-for i in range (1,11):
-    print(i)
+from fastapi import FastAPI , HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    try:
+        return{"message" : "TNAKT"}
+    except Exception as e:
+        print(f'Exception : {e}')
+        raise HTTPException(status_code = 500 , detail = str(e))
+    
+@app.get("/")
+async def Bienvenue():
+    try:
+        return{"message" : "TNAKT x2"}
+    except Exception as e:
+        print(f'Exception : {e}')
+        raise HTTPException(status_code = 500 , detail = str(e))
