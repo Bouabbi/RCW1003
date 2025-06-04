9ccfc30f9010381dfda93a6f30bc4a600db7905b
APACHE <bouabbimostapha@gmail.com>
2025-06-04 15:31:25 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'http://localhost:8000'
+#url = 'http://localhost:8000'
+url = 'https://rcw1003--inst01-bubbh6gbgrhcb4ad.canadaeast-01.azurewebsites.net/'
 
 response = requests.get(url)
 response =response.json()
